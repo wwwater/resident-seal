3ac4a5f8d34dbc1cf3b4d07fe2086e58db336bff
Alexey Lebedev <alisey.login@gmail.com>
2014-11-23 00:29:57 +0200

    Set CWD to the location of executable, so we can use relative paths for images

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setOrganizationName("Seal Adoration Society");
     app.setApplicationName("Abode of Seals");
+
+    QDir::setCurrent(QCoreApplication::applicationDirPath());
+
     MainWindow mainWin;
     mainWin.show();
     return app.exec();
